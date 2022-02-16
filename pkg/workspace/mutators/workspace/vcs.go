9f82acb73d90b11e839349004e091e047ebd7f20
Ivan Chebykin <ivan@chebykin.org>
2022-02-17 01:04:13 +0300

    fix

--- pkg/workspace/mutators/workspace/vcs.go
+++ pkg/workspace/mutators/workspace/vcs.go
@@ -1,9 +1,53 @@
 package workspace
 
 import (
+	"time"
+
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/mify-io/mify/pkg/workspace/mutators"
 )
+
+func getCommitOpts(repo *git.Repository) (git.CommitOptions, error) {
+	o := git.CommitOptions{}
+	cfg, err := repo.ConfigScoped(config.SystemScope)
+	if err != nil {
+		return o, err
+	}
+
+	if cfg.Author.Email != "" && cfg.Author.Name != "" {
+		o.Author = &object.Signature{
+			Name:  cfg.Author.Name,
+			Email: cfg.Author.Email,
+			When:  time.Now(),
+		}
+	}
+
+	if cfg.Committer.Email != "" && cfg.Committer.Name != "" {
+		o.Committer = &object.Signature{
+			Name:  cfg.Committer.Name,
+			Email: cfg.Committer.Email,
+			When:  time.Now(),
+		}
+	}
+
+	if o.Author == nil && cfg.User.Email != "" && cfg.User.Name != "" {
+		o.Author = &object.Signature{
+			Name:  cfg.User.Name,
+			Email: cfg.User.Email,
+			When:  time.Now(),
+		}
+	}
+	if o.Author == nil {
+		o.Author = &object.Signature{
+			Name:  "Mify",
+			Email: "support@mify.io",
+			When:  time.Now(),
+		}
+	}
+	return o, nil
+}
 
 func InitGit(mutContext *mutators.MutatorContext) error {
 	repo, err := git.PlainInit(mutContext.GetDescription().BasePath, false)
@@ -14,6 +58,12 @@ func InitGit(mutContext *mutators.MutatorContext) error {
 	if err != nil {
 		return err
 	}
-	_, err = tree.Commit("Initial commit", &git.CommitOptions{})
+
+	opts, err := getCommitOpts(repo)
+	if err != nil {
+		return err
+	}
+
+	_, err = tree.Commit("Initial commit", &opts)
 	return err
 }
